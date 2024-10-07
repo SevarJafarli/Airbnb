7397f1e85bf9f3a9dfc08fd702dda8c677008854
008458 <008458@ibar.int>
2024-10-07 08:36:25 +0400

    add listing item detail view

---
+++ Airbnb/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  Airbnb
+//
+//  Created by Sevar Jafarli on 06.10.24.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
